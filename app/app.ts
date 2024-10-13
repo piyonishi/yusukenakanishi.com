1f60ebb2fdf4d3f1d181bbb0ffbe54f9c5462bf6
中西祐介 <yusuke.nakanishi@gmail.com>
2024-10-14 02:02:28 +0900

    Add Zod for request validation

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
-import { expensesRoute } from './routes/expenses';
+import { postsRoute } from './routes/posts';
 
 const app = new Hono();
 
@@ -10,6 +10,6 @@ app.get('/test', (c) => {
   return c.json({ message: 'test' });
 });
 
-app.route('/api/expenses', expensesRoute);
+app.route('/api/v1/posts', postsRoute);
 
 export default app;
