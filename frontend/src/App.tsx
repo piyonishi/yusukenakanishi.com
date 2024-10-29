2cd599f34fd2f383f0d7bfde0110c169a06b2bae
中西祐介 <yusuke.nakanishi@gmail.com>
2024-10-29 17:22:06 +0900

    simplify tsconfig structure in frontend directory

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,29 +2,45 @@ import { useState } from 'react';
 import viteLogo from '/vite.svg';
 import reactLogo from './assets/react.svg';
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button size={"sm"} onClick={() => setCount((count) => count + 1)} className="mr-1">UP</Button>
-        <Button size={"sm"} onClick={() => setCount((count) => count - 1)}>DOWN</Button>
-        <p>count is {count}</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Card className="w-[580px] m-auto">
+        <CardHeader>
+          <CardTitle>Total value</CardTitle>
+          <CardDescription className="flex">
+            Total value of counter.
+            <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+              <img src={viteLogo} className="inline-block ml-2" alt="Vite logo" width={14} />
+            </a>
+            <a href="https://react.dev" target="_blank" rel="noreferrer">
+              <img src={reactLogo} className="inline-block ml-1" alt="React logo" width={14} />
+            </a>
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className=" flex items-center space-x-4 rounded-md border p-4 text-lg">
+            {count}
+          </div>
+        </CardContent>
+        <CardFooter>
+          <div className="flex gap-3 w-full">
+            <Button size={"sm"} onClick={() => setCount((count) => count - 1)} className="w-full">DOWN</Button>
+            <Button size={"sm"} onClick={() => setCount((count) => count + 1)} className="w-full mr-1">UP</Button>
+          </div>
+        </CardFooter>
+      </Card>
     </>
   );
 }
